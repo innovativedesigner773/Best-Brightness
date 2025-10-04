6c6d5b17409b2b4994ddc26be2fc7412fede2e6a
Jabulani00 <jabulanimosesgwala@gmail.com>
2025-10-04 15:24:10 +0200

    all

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 
   import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react-swc';
+  import react from '@vitejs/plugin-react';
   import path from 'path';
 
   export default defineConfig({
@@ -54,6 +54,14 @@
     build: {
       target: 'esnext',
       outDir: 'build',
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+            ui: ['@radix-ui/react-dialog', '@radix-ui/react-dropdown-menu']
+          }
+        }
+      }
     },
     server: {
       port: 3000,
